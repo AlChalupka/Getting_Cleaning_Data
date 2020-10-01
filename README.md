312f5d886737c6eea0b9037bbea051d8d0f53b78
AlChalupka <chalupka.alena@gmail.com>
2020-10-01 12:58:25 +0200

    adding description of data_analysis.R

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Getting_Cleaning_Data
-Repository for the final assignment of the "Getting and Cleaning Data" - course in the Data Science specialization by Johns Hopkins on Coursera.   
+# Getting and Cleaning Data Course Project 
+
+* This is the project for the "Getting and Cleaning Data" course on Coursera in the data science specialisation by Johns Hopkins University
+* The R-script `data_analysis.R` does the following:
+
+  1. Checking if the data has already been downloaded, if not a directory to store the data is created and       the data is downloaded 
+  2. The train and test data sets are combined for the observations X, the activity y and the subject,           respectivly
+  3. The observations (features) are named according the file `features.txt` accompanying the data
+  4. Extracting the observations on the mean and standard deviation for each measurement
+  5. Replaces numeric labels for each activity with descriptiv activity names 
+  6. Appropiatly labels the measurments, by removing `-` and `()` and capitalizing `Mean` and `Std`
+  7. Creates a tidy data set with the average of each measurment for each activity and each subject; the         result is saved in `tidy_data.txt`
+
+
+
--- data_analysis.R
+++ data_analysis.R
@@ -35,32 +35,33 @@ X_data <- rbind(X_train, X_test)
 y_data <- rbind(y_train, y_test)
 subject_data <- rbind(subject_train, subject_test)
 
+## 3. Naming the features 
 # reading in features names
 features <- read.table(paste0(dataDir, "/UCI HAR Dataset/features.txt"))
 # naming features in the X_data 
 names(X_data) <- features$V2
 
 
-## 3. Extract measurements on the mean and the standard deviation for each measurment 
+## 4. Extract measurements on the mean and the standard deviation for each measurment 
 extracted_col <- grep("mean\\(|std\\(", names(X_data))
 data <- X_data[extracted_col]
 
 
-## 4. Using describtive activity names 
+## 5. Using describtive activity names 
 labels <- read.table(paste0(dataDir, "/UCI HAR Dataset/activity_labels.txt"), stringsAsFactors = FALSE)
 data$activity <- factor(y_data$V1, levels=labels$V1, labels=labels$V2)
 data$subject <- as.factor(subject_data$V1)
 
-## 5. Appropiatly name the variables 
+## 6. Appropiatly name the variables 
 names(data) <- gsub("-", "", names(data))
 names(data) <- gsub("\\(\\)", "", names(data))
 names(data) <- gsub("mean", "Mean", names(data))
 names(data) <- gsub("std", "Std", names(data))
 
 
-## 6. Create a data set with the average of each variable for each activity and each subject 
+## 7. Create a data set with the average of each variable for each activity and each subject 
 dataMelt <- melt(data, id = c("activity", "subject"))
 tidy_data <- dcast(dataMelt, activity + subject ~ variable, mean)
 
-## 7. Saving the tidy data set 
-write.table(tidy_data, file = "tidy_data.txt")
+#Saving the tidy data set 
+write.table(tidy_data, file = "tidy_data.txt", row.names = FALSE)
